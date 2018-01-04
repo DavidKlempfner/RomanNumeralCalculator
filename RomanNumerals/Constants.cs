fb13bfa5dd80608165ea6fd59397aa776431e425
AVANADE-CORP\david.klempfner <dklempfner@gmail.com>
2018-01-04 16:11:04 +1100

    Modified to work with any number between 1000000 and Int32.MaxValue

---
+++ RomanNumerals/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    public static class Constants
+    {
+        public const int MinimumValueComputable = 1;
+        public const int MaximumValueComputable = Int32.MaxValue;
+    }
+}
